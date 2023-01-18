cd4c177d35bf64985f4d0461648ad008a559f7dd
Endijs Dārznieks <endijs.darznieks@accenture.com>
2023-01-18 16:27:25 +0200

    Task 7

---
+++ task7/script.js
@@ -0,0 +1,19 @@
+const myTable = document.getElementById("myTable")
+
+function createTable(){
+    myTable.innerHTML = ""
+
+    const rows = Number(prompt("Rows: "))
+    const columns = Number(prompt("Columns: "))
+
+    if(rows && columns && Number.isInteger(rows) && Number.isInteger(columns)){
+        for (let i = 0; i < rows; i++) {
+            myTable.insertRow(i)
+            for (let j = 0; j < columns; j++) {
+                let cell = myTable.rows[i].insertCell(j)
+                cell.innerHTML = `Rows-${i+1} Columns-${j+1}`
+            }
+        }
+    }else
+        alert("Something went wrong!")
+}
