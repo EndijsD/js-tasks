67e865a9fafa206a36dc6275bef127e5e9d45561
Endijs Dārznieks <endijs.darznieks@accenture.com>
2023-01-19 15:18:49 +0200

    Task 10

---
+++ task10/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn")
+const form = document.getElementById("form")
+
+btn.addEventListener('click', function(){
+    let answer
+    form.childNodes.forEach(item => item.name == "radius" ? answer = (4/3 * Math.PI * Math.pow(item.value, 3)).toFixed(4) : item.name == "volume" ? item.value = answer : false)
+})
